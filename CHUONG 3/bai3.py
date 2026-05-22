576e4c36f4bfb6ad4232bdafc13db9727eb59beb
PhamCongHung0109 <phamconghunq@gmail.com>
2026-05-22 18:35:48 +0700

    Add files via upload

---
+++ CHUONG 3/bai3.py
@@ -0,0 +1,6 @@
+def msg():
+    a = 10
+    print("gia tri cua a la:", a)
+    return
+
+msg()
---
+++ CHUONG 3/bai4.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+def msg():
+    print("gia tri cua a la:", a)
+    print("gia tri cua b la:", b)
+    return
+msg()
+print(b)
